309536d1ecca918a220b7aca726586b9afe4d235
Aluno Lab. 08 <aluno08@aeelabs.edu.br>
2023-04-11 21:51:41 -0300

    feat: first route

--- src/Animes/dto/create-anime.dto.ts
+++ src/Animes/dto/create-anime.dto.ts
@@ -1,8 +1,30 @@
+import { Transform } from "class-transformer";
+import { IsDate, IsNumber, IsOptional, IsString, Max, MaxLength, Min } from "class-validator";
+
 export class createAnimeDTO {
+  @IsString()
+  @MaxLength(50)
   name: string;
+
+  @IsDate()
+  @Transform((params)=> new Date(params.value))
   relase_date: Date;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(500)
   descrition?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Max(10)
+  @Min(1)
   avaliation?: number;
+
+  @IsString()
+  @MaxLength(50)
   author: string;
+
+  @IsNumber()
   genre_id: number;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AnimesModule } from './animes.module';
+import { HttpException, HttpStatus, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AnimesModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (err) => {
+        console.log("ERROR", err);
+        return new HttpException(
+        'Dados obrigatorios não informados ou invalidos',
+        HttpStatus.BAD_REQUEST,
+      );
+      },
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
